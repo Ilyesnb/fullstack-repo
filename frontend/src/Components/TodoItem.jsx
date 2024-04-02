9899eae78654a99f8328ca8838cb1b36a4238fa9
ilyesnb <ilyesnabi9@gmail.com>
2024-04-02 01:20:55 +0100

    latest commit

--- frontend/src/Components/TodoItem.jsx
+++ frontend/src/Components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import trashIcon from "../images/delete-bin-line.png"
 import axios from "axios"
-const App_BASE = "https://fullstack-repo.onrender.com"
+const App_BASE = "https://fullstack-repo-1.onrender.com"
 const TodoItem = ({ title, id,onDelete }) => {
     const handleDelete = () => {
         console.log("Deleting todo with ID:", id)
